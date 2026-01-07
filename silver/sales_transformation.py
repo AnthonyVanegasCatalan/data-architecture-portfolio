28a306877894e3ebc2b28be46d0c1dfa92a308c8
Anthony Vanegas <158592804+AnthonyVanegasCatalan@users.noreply.github.com>
2026-01-07 13:55:24 -0600

    Create sales_transformation.py

---
+++ silver/sales_transformation.py
@@ -0,0 +1,9 @@
+def transform_sales(df_raw, dim_sku):
+    """
+    Applies business rules:
+    - Status normalization
+    - SKU standardization
+    - Currency & tax calculation
+    - Dimensional enrichment
+    """
+    pass
